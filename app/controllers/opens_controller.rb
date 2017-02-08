3fd5bb89d700e3feb24528844710de14db402626
Omar Sahyoun <osahyoun@gmail.com>
2017-02-08 11:09:12 +0000

    Return a hidden pixel

--- app/controllers/opens_controller.rb
+++ app/controllers/opens_controller.rb
@@ -3,6 +3,10 @@ class OpensController < ApplicationController
 
   def track
     Open.track(@campaign, recipient_id: params[:recipient_id])
+    send_data(
+      Base64.decode64("R0lGODlhAQABAPAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="),
+      type: "image/gif", disposition: "inline"
+    )
   end
 
 
